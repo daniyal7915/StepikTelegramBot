17ffd6d7aa37049ddb8967ac8a0c7c10c0ef5f2c
daniyal7915 <daniyal.isin79@gmail.com>
2021-03-20 11:46:18 -0300

    back to initial+Heroku PGDB

--- week4_test.py
+++ week4_test.py
@@ -109,7 +109,8 @@ while attempt <= 3:
     attempt += 1
     try:
         connection = psycopg2.connect(database=os.environ['DB'], user=os.environ['DB_USER'],
-                                      password=os.environ['DB_PASSWORD'])
+                                      password=os.environ['DB_PASSWORD'], host=os.environ['DB_HOST'],
+                                      port=os.environ['DB_PORT'])
         cursor = connection.cursor()
         @bot.message_handler(func=lambda message: show_state(message.from_user.id) == INIT_STATE)
         def init(message):
